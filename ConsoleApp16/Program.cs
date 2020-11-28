242da12aa4dfad9d0a804b8590b46ef4fe829bd5
ALexKhBohko <profirentfirm@gmail.com>
2020-11-28 21:13:33 +0200

    ЗАЛИВАЕМ БЛЯДЬ

--- ConsoleApp16/Program.cs
+++ ConsoleApp16/Program.cs
@@ -7,6 +7,11 @@ namespace ConsoleApp16
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!"); Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.SetWindowSize(40,30);
+            Console.SetBufferSize(40,30);
+            Console.ReadLine();
         }
     }
 }
